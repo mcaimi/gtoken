5bef8c6486fcaccd171bfc4cbc53314fd831f5ef
Marco Caimi <mcaimi@redhat.com>
2023-08-24 12:06:48 +0200

    More refactor: database subcommand

--- cmd/gtoken/database/database.go
+++ cmd/gtoken/database/database.go
@@ -5,8 +5,9 @@ import (
   "fmt"
 
   "github.com/spf13/cobra"
-  "github.com/mcaimi/gtoken/pkg/common"
   "github.com/mcaimi/gtoken/pkg/gtoken"
+  "github.com/mcaimi/gtoken/cmd/gtoken/styles"
+  "github.com/jedib0t/go-pretty/v6/table"
 )
 
 var (
@@ -23,17 +24,21 @@ var (
     Long: "Show information about the Account Database",
     Aliases: []string{"s", "stat", "info", "show"},
     Run: func(cmd *cobra.Command, args []string) {
-      if dbPath, err := common.GetAccountsDB(); err == nil {
-        fmt.Printf("Database Location: [%s]\n", dbPath);
+      var d gtoken.Database;
+      var err error;
 
-        // count entries
-        if d, err := gtoken.OpenAccountDB(dbPath); err == nil {
-          fmt.Printf("\t-> Currently, the Database contains [%d] configured entries.\n", d.Count());
-        }
-      } else {
+      if d, err = gtoken.ReadAccountDb(); err != nil {
         fmt.Printf("gtoken status error: [%s]\n", err);
         os.Exit(1);
       }
+
+      // format and print database status
+      tbl := table.NewWriter();
+      tbl.SetOutputMirror(os.Stdout);
+      tbl.SetStyle(styles.GetStyle());
+      tbl.AppendHeader(table.Row{"DB Path", "Entries"});
+      tbl.AppendRow(table.Row{d.DbFilePath, d.Count()});
+      tbl.Render();
 
       // ok
       os.Exit(0);
